3b354777afd43ca8136edcda446df3ecad881601
M Jerez <mjesusjerezj@gmail.com>
2022-01-31 19:48:23 -0300

    se creó un contenedor para cada película y su título

--- src/main.js
+++ src/main.js
@@ -1,55 +1,28 @@
 //import { example } from './data.js';
 import data from './data/ghibli/ghibli.js';
 
-//let dataghibli = data.films;
-//const posterpelicula = document.getElementById("posterfilms")
 
-//despligue de posters
-//let displayposter = document.getElementById ("posterfilms");
+const dataghibli = data.films
 
-  /*   document.getElementById("filtervaloracion").addEventListener("click",() => {
-        for (let i = 0; i < dataghibli.length; i++) {
-            console.log(dataghibli[i].id);
+const containerposter = document.getElementById("poster")
 
-            let imagenpelicula = document.createElement('img')
-            imagenpelicula.appendChild(dataghibli[i].id);
+   for(let i=0;i<dataghibli.length;i++){
+                        
+            
+     let ficha = document.createElement ("div");
+       let imagen = document.createElement("img");
+       let nombre = document.createElement("p");
+
+            
+        imagen.setAttribute("src",dataghibli[i].poster);
+        nombre.setAttribute("id", "tarjeta");
+        ficha.setAttribute ("class", "fichita")
+        nombre.innerHTML = dataghibli[i].title;
+
+       ficha.appendChild (imagen);
+        containerposter.appendChild(ficha);
+        ficha.appendChild (nombre)
+        //poster.appendChild (leyenda)
+           
+        
         }
- */
-        
-
-        // console.log(data.films);
-
-        let dataghibli = data.films;
-        //let posterpelicula = data.films
-        /* let displayposter = document.getElementById ("posterfilms");
-        
-
-        for (let i=0; i <dataghibli.length; i++) {
-            let pelicula = document.createElement("div")
-            let imagenpelicula = document.createElement("img");
-
-            
-            imagenpelicula.setAttribute("src" posterpelicula[i].imagenpelicula);
-            displayposter.appendChild (pelicula);
-           displayposter.appendChild (imagenpelicula);
-
-        }
- */
-        
-        for(let i=0;i<dataghibli.length;i++){
-            
-            
-            //document.getElementById("poster").innerHTML+=dataghibli[i].poster
-            let ficha = document.createElement ("flexbox")
-            let imagen = document.createElement("img");
-            //let leyenda = document.createElement("p")
-
-            
-            imagen.setAttribute("src",dataghibli[i].poster);
-            poster.appendChild (imagen)
-            poster.appendChild(ficha)
-            //poster.appendChild (leyenda)
-           // leyenda.innerHTML="dataghibli[i].title"
-            //imagenpelicula.setAttribute("src" dataghibli[i].poster);
-            
-          }
