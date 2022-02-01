fa7eee37a1fb5a43d696b4661d72bfce8fbd2b38
maria vera <asuvera93@gmail.com>
2022-02-01 07:52:42 -0300

    agrego select score

--- src/main.js
+++ src/main.js
@@ -5,12 +5,24 @@ let dataghibli = data.films;
 for(let i=0;i<dataghibli.length;i++){
                     
   //document.getElementById("poster").innerHTML+=dataghibli[i].poster
-  let ficha = document.createElement ("flexbox")
+  let ficha = document.createElement ("flexbox");
   let imagen = document.createElement("img");
-  let leyenda = document.createElement("p")
+  let leyenda = document.createElement("p");
+  
+  let titulo= document.createTextNode (dataghibli[i].title);
+  
 
   imagen.setAttribute("src",dataghibli[i].poster);
-  poster.appendChild (imagen)
+  //titulo.setAttribute ("style");
+  
   poster.appendChild(ficha)
+  poster.appendChild (imagen)
+  poster.appendChild(titulo)
+ 
 }
+
+
+
+let filterDirector = dataghibli.filter( directores =>"director"=="Hayao Miyazaki", filterDirector)
+console.log=(filterDirector);
 
