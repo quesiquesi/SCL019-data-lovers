352455ffb0f653927da07a1dcafbdada8ca49b18
M Jerez <mjesusjerezj@gmail.com>
2022-02-03 20:11:13 -0300

    prueba

--- src/data.js
+++ src/data.js
@@ -1,9 +1,14 @@
 // estas funciones son de ejemplo
-
+/* 
 export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+}; */
+
+export const filtrodirector = (data, valor) =>{
+const directoresghibli = data.filter(eldirector =>(eldirector.director===valor))
+return directoresghibli
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-//import { example } from './data.js';
+import { filtrodirector } from './data.js' 
 import data from './data/ghibli/ghibli.js';
 
 
-const dataghibli = data.films
+const dataghibli = data.films 
+
  const containerposter = document.getElementById("poster")
 
-   for(let i=0;i<dataghibli.length;i++){
+  const displaydata = (dataghibli) =>{  
+
+    poster.innerHTML = ''
+  
+     for(let i=0;i<dataghibli.length;i++){
                         
             
        let ficha = document.createElement ("div");
@@ -22,12 +27,60 @@ const dataghibli = data.films
        containerposter.appendChild(ficha);
        ficha.appendChild (nombre)
        
-           
-        
-        } 
- 
-//filtro según director
+      }  
+      }  
+    
+       
+      window.addEventListener('load',()=>{
+         displaydata(dataghibli)
+       })
 
- document.getElementById("filterdirector").addEventListener("change")
-  let pelishayao = dataghibli.filter(pelicula=> pelicula.director == "Hayao Miyazaki")
-console.log (pelishayao)
+        
+ 
+//filtro según director que se muestre en html
+
+/* const filtroghibli = document.getElementById('filterdirector');                 //se crea var, que llame desde el select creado, select que tiene 2 opciones
+ 
+filtroghibli.addEventListener('change',()=>{                                        // evento que se dispara y usa los datos ligados a la opcion dando respuesta segun seleccion y que realice la sgte funcion 
+    var selectedirector = filtroghibli.options[filtroghibli.selectedIndex].value;     // se crea var que tome las opciones desde la const aletasfiltro y segun  el valor seleccionado (F-M)
+    console.log(selectedirector)
+
+    let resultado = filtrodirector(directores,selectdirector)                              // se crea var resultado que tiliza la f de filtrar en los parametrso de la data y el valor de var creada em L41
+    displaydata(resultado)                                                                  // se usa la funcion card para que nos cree el formado de var resultados 
+    console.log(resultado)
+
+
+
+
+ */
+
+
+
+
+
+
+ const filtroghibli = document.getElementById('filterdirector');
+
+filtroghibli.addEventListener('change',()=>{
+
+var selectdirector = filtroghibli.options[filtroghibli.selectedIndex].value
+
+console.log(selectdirector)
+
+let resultado = filtrodirector (dataghibli, selectdirector)
+displaydata(resultado)
+console.log(resultado)
+})
+  
+
+
+
+
+
+ 
+ /*  let pelishayao = dataghibli.filter(pelicula=> pelicula.director == "Hayao Miyazaki")
+  console.log (pelishayao) */
+  
+ 
+
+
